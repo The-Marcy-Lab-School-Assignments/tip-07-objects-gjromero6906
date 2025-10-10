a37303d1dc79cc45433907921bdbbe1a46700111
gjromero6906 <gjr6906@gmail.com>
2025-10-10 10:32:49 -0400

    classprocess

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 //write your code here
+const countWordFrequency =(sentence)=>{
+    let wordsArr =sentence.split(" ");
+    let obj = {};
+    for (let i =0;i<wordsArr.length;i++){
+        obj[word[i]] = 1;
+    }
+}
